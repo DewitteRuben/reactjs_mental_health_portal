eb2819aa6aa282fe66ece0378eea391d94c4ee78
Ruben Dewitte <ruben.dewitte@proximus.be>
2020-05-24 22:00:24 +0200

    feat: added css baseline

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { StylesProvider } from "@material-ui/core/styles";
 import Home from "./pages/Home";
@@ -6,6 +8,7 @@ import Home from "./pages/Home";
 function App() {
   return (
     <Router>
+      <CssBaseline />
       <StylesProvider injectFirst>
         <Switch>
           <Route path="/">
