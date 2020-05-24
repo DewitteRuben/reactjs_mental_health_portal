68b139e4e759c749729abc0210a0facb153ad437
Ruben Dewitte <ruben.dewitte@proximus.be>
2020-05-24 21:44:23 +0200

    feat: add stylesprovider for styled components support

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { StylesProvider } from "@material-ui/core/styles";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
+      <StylesProvider injectFirst>
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </StylesProvider>
     </Router>
   );
 }
