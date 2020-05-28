3816625717c2ec3e44d542c1c5e248411bbb7b64
Ruben Dewitte <ruben.dewitte@proximus.be>
2020-05-28 23:34:20 +0200

    feat: api boilerplate

---
+++ src/api/index.ts
@@ -0,0 +1,30 @@
+import ApiError from "../errors/ApiError";
+import _ from "lodash";
+
+const request = async (
+  endpoint: string,
+  requestInit?: RequestInit,
+  parseAs: "json" | "string" = "json"
+) => {
+  if (!endpoint.includes("/")) {
+    throw new Error("The endpoint must be prepended with /");
+  }
+
+  const options = _.merge(
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    },
+    requestInit
+  );
+
+  const resp = await fetch(`${process.env.REACT_APP_BASE_API_LOCAL}${endpoint}`, options);
+  const data = await (parseAs === "json" ? resp.json() : resp.text());
+  if (resp.status !== 200) {
+    throw new ApiError(data.error.message, resp.status);
+  }
+  return data;
+};
+
+export { request };
