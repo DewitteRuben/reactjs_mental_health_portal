93d9e6af1473ba726884b872efe6944214f469e7
Ruben Dewitte <ruben.dewitte@proximus.be>
2020-05-29 13:31:27 +0200

    feat: basic register / login functionality

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,29 @@
 import React from "react";
 import { Container, Box } from "@material-ui/core";
-import LoginForm from "../components/LoginForm";
+import LoginForm, { ILoginFormData } from "../components/LoginForm";
+import { auth } from "../api/authApi";
+import { AuthContext } from "../store/authStore";
+import { Redirect } from "react-router-dom";
 
 const Home: React.FC = () => {
+  const { state, dispatch } = React.useContext(AuthContext);
+
+  const handleLogin = async (data: ILoginFormData) => {
+    const { email, password } = data;
+    try {
+      const resp = await auth(email, password);
+      const { token } = resp;
+      dispatch({
+        type: "UPDATE",
+        payload: { token, authenticated: true, status: "AUTHENTICATED" },
+      });
+    } catch (error) {}
+  };
+
+  if (state.authenticated) {
+    return <Redirect to="/dashboard" />;
+  }
+
   return (
     <Container>
       <Box
@@ -11,7 +32,7 @@ const Home: React.FC = () => {
         height="100%"
         display="flex"
       >
-        <LoginForm />
+        <LoginForm onSubmit={handleLogin} />
       </Box>
     </Container>
   );
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,8 +1,21 @@
 import React from "react";
 import { Container, Box } from "@material-ui/core";
-import RegisterForm from "../components/RegisterForm";
+import RegisterForm, { IRegisterFormData } from "../components/RegisterForm";
+import { registerProfessional } from "../api/authApi";
 
 const Register: React.FC = () => {
+  const handleOnRegister = async (formData: IRegisterFormData) => {
+    const { confirmPassword, ...other } = formData;
+    const professional = { ...other };
+
+    try {
+      const response = await registerProfessional(professional);
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <Container>
       <Box
@@ -11,7 +24,7 @@ const Register: React.FC = () => {
         height="100%"
         display="flex"
       >
-        <RegisterForm onSubmit={(item) => console.log(item)} />
+        <RegisterForm onSubmit={handleOnRegister} />
       </Box>
     </Container>
   );
