69bc3a10e7e8db4ccf0d19d66eb42e67fc849994
Ruben Dewitte <ruben.dewitte@proximus.be>
2020-05-31 22:48:22 +0200

    feat: update auth api endpoints

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -1,11 +1,23 @@
 import { request } from ".";
 
-interface IProfessional {
+interface INewProfessional {
   email: string;
   firstName: string;
   lastName: string;
   password: string;
   birthDate: Date;
+}
+
+export interface IClient {
+  userId: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  birthDate: string;
+}
+
+export interface IProfessional extends INewProfessional {
+  clients: IClient[];
 }
 
 const auth = (email: string, password: string) => {
@@ -15,11 +27,18 @@ const auth = (email: string, password: string) => {
   });
 };
 
-const registerProfessional = (credentials: IProfessional) => {
+const refreshToken = () => {
+  return request("/auth/professional/refresh", {
+    method: "POST",
+    credentials: "include",
+  });
+};
+
+const registerProfessional = (credentials: INewProfessional) => {
   return request("/professional", {
     method: "POST",
     body: JSON.stringify({ ...credentials }),
   });
 };
 
-export { auth, registerProfessional };
+export { auth, registerProfessional, refreshToken };
