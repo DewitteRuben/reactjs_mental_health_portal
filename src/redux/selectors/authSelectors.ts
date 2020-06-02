86113f031331a15b65731dc37df3060c0752cc6c
Ruben Dewitte <ruben.dewitte@proximus.be>
2020-06-02 22:17:34 +0200

    feat: client by id selector helper (redux)

--- src/redux/selectors/authSelectors.ts
+++ src/redux/selectors/authSelectors.ts
@@ -9,9 +9,15 @@ const selectAuthError = (state: IRootState) => state.auth.error;
 
 const selectAuthStatus = (state: IRootState) => state.auth.status;
 
+const selectClientById = (userId: string) => (state: IRootState) =>
+  state.auth.professional?.clients.filter(
+    (client) => client.userId === userId
+  )[0];
+
 export {
   selectAuthenticated,
   selectProfessionalClients,
   selectAuthError,
   selectAuthStatus,
+  selectClientById,
 };
