9d43b0324ed5564dcd86f0ff216fb24f5d7205ce
Ruben Dewitte <ruben.dewitte@proximus.be>
2020-06-02 15:55:18 +0200

    feat: added Smiley component

---
+++ src/components/Smiley.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import Image from "material-ui-image";
+
+export type SmileyType = "verybad" | "bad" | "moderate" | "good" | "verygood";
+
+interface SmileyProps {
+  type: SmileyType;
+  size?: number;
+}
+
+interface SmileyState {
+  src?: string;
+}
+
+const DEFAULT_SMILEY_SIZE = 56;
+
+class Smiley extends React.Component<SmileyProps, SmileyState> {
+  state: SmileyState = { src: undefined };
+
+  async componentDidMount() {
+    const { type } = this.props;
+    const { default: src } = await import(`../images/smiley_${type}.png`);
+    this.setState({ src });
+  }
+
+  render() {
+    const { src } = this.state;
+    const { size } = this.props;
+
+    return (
+      <div
+        style={{
+          width: size || DEFAULT_SMILEY_SIZE,
+          height: size || DEFAULT_SMILEY_SIZE,
+        }}
+      >
+        <Image
+          style={{
+            width: size || DEFAULT_SMILEY_SIZE,
+            height: size || DEFAULT_SMILEY_SIZE,
+            backgroundColor: "rgba(0, 0, 0, 0)",
+          }}
+          imageStyle={{}}
+          disableSpinner
+          src={src || ""}
+        />
+      </div>
+    );
+  }
+}
+
+export default Smiley;
