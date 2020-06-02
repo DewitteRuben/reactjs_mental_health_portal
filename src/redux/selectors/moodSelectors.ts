99a96f5d1a44907838f90cec59fdd21e6be1f7fe
Ruben Dewitte <ruben.dewitte@proximus.be>
2020-06-02 22:14:07 +0200

    feat: added helper selectors for mood store

--- src/redux/selectors/moodSelectors.ts
+++ src/redux/selectors/moodSelectors.ts
@@ -1,5 +1,21 @@
 import { IRootState } from "../store";
+import { IMoodEntry } from "../reducers/moodReducer";
 
 const selectMoodEntries = (state: IRootState) => state.mood.entries;
 
-export { selectMoodEntries };
+const selectMoodEntriesDateAsc = (state: IRootState) =>
+  state.mood.entries.sort(
+    (a: IMoodEntry, b: IMoodEntry) =>
+      new Date(b.date).getTime() - new Date(a.date).getTime()
+  );
+
+const selectMoodStatus = (state: IRootState) => state.mood.status;
+
+const selectMoodLoading = (state: IRootState) => state.mood.loading;
+
+export {
+  selectMoodEntries,
+  selectMoodStatus,
+  selectMoodLoading,
+  selectMoodEntriesDateAsc,
+};
