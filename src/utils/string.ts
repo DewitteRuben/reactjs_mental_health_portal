341c5667dd34f30f3a95b0a9edd6bad00aba0eb8
Ruben Dewitte <ruben.dewitte@proximus.be>
2020-06-02 15:56:55 +0200

    feat: added helper to capitalize camcel case string

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,8 +1,16 @@
+import _ from "lodash";
+
 const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 const splitCamelCase = (text: string) =>
   text.replace(/([a-z])([A-Z])/g, "$1 $2").split(" ");
 
+const capitalizeCamelCase = (text: string) => {
+  return splitCamelCase(text)
+    .map((string) => _.capitalize(string))
+    .join(" ");
+};
+
 const isValidEmail = (email: string) => emailRegex.test(email);
 
-export { splitCamelCase, isValidEmail };
+export { splitCamelCase, isValidEmail, capitalizeCamelCase };
