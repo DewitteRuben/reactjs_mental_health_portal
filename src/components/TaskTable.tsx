87924bdd9156de9f06264b16fff04a1c1c89d69a
Ruben Dewitte <ruben.dewitte@proximus.be>
2020-06-04 00:56:31 +0200

    feat: component to display / manipulate tasks

--- src/components/TaskTable.tsx
+++ src/components/TaskTable.tsx
@@ -17,6 +17,13 @@ import {
 import AddIcon from "@material-ui/icons/Add";
 import DeleteIcon from "@material-ui/icons/Delete";
 import EditIcon from "@material-ui/icons/Edit";
+import AddTaskDialog from "./AddTaskDialog";
+import { IAddTaskFormData } from "./AddTaskForm";
+import { connect } from "react-redux";
+import { ITask } from "../redux/reducers/taskReducer";
+import { ThunkDispatch } from "redux-thunk";
+import { fetchAddTask } from "../redux/actions/taskActions";
+import moment from "moment";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -27,55 +34,95 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
-const TaskTable = () => {
-  const styles = useStyles();
+interface ITaskTableProps {
+  tasks: ITask[];
+  userId: string;
+  fetchAddTask: (title: string, description: string, userId: string) => void;
+}
 
-  return (
-    <>
-      <Toolbar className={styles.toolbar}>
-        <Button startIcon={<AddIcon />}>Assign Task</Button>
-      </Toolbar>
-      <TableContainer>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell align="right">
-                <TableSortLabel>Title</TableSortLabel>
-              </TableCell>
-              <TableCell align="right">
-                <TableSortLabel>Description</TableSortLabel>
-              </TableCell>
-              <TableCell align="right">
-                <TableSortLabel>Has been completed</TableSortLabel>
-              </TableCell>
-              <TableCell align="right">
-                <TableSortLabel>Date of completion</TableSortLabel>
-              </TableCell>
-              <TableCell align="right">Actions</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            <TableRow>
-              <TableCell align="right">Activity</TableCell>
-              <TableCell align="right">Clean up your room today.</TableCell>
-              <TableCell align="right">Yes</TableCell>
-              <TableCell align="right">
-                {new Date().toLocaleDateString()}
-              </TableCell>
-              <TableCell align="right">
-                <IconButton>
-                  <EditIcon />
-                </IconButton>
-                <IconButton>
-                  <DeleteIcon />
-                </IconButton>
-              </TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </>
-  );
-};
+const TaskTable: React.FC<ITaskTableProps> = React.memo(
+  ({ tasks, userId, fetchAddTask }) => {
+    const styles = useStyles();
 
-export default TaskTable;
+    const [visible, setVisibility] = React.useState(false);
+
+    const displayModal = React.useCallback(() => {
+      setVisibility(true);
+    }, []);
+
+    const handleOnSubmit = React.useCallback(
+      (formData: IAddTaskFormData) => {
+        const { title, description } = formData;
+        fetchAddTask(title, description, userId);
+      },
+      [fetchAddTask, userId]
+    );
+
+    return (
+      <>
+        <Toolbar className={styles.toolbar}>
+          <Button onClick={displayModal} startIcon={<AddIcon />}>
+            Assign Task
+          </Button>
+        </Toolbar>
+        <TableContainer>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell align="right">
+                  <TableSortLabel>Title</TableSortLabel>
+                </TableCell>
+                <TableCell align="right">
+                  <TableSortLabel>Description</TableSortLabel>
+                </TableCell>
+                <TableCell align="right">
+                  <TableSortLabel>Has been completed</TableSortLabel>
+                </TableCell>
+                <TableCell align="right">
+                  <TableSortLabel>Date of completion</TableSortLabel>
+                </TableCell>
+                <TableCell align="right">Actions</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {tasks.map((task) => (
+                <TableRow key={task.taskId}>
+                  <TableCell align="right">{task.title}</TableCell>
+                  <TableCell align="right">{task.description}</TableCell>
+                  <TableCell align="right">
+                    {task.completed ? "Yes" : "No"}
+                  </TableCell>
+                  <TableCell align="right">
+                    {task.dateOfCompletion
+                      ? moment(task.dateOfCompletion).format("LL")
+                      : "Not yet completed"}
+                  </TableCell>
+                  <TableCell align="right">
+                    <IconButton>
+                      <EditIcon />
+                    </IconButton>
+                    <IconButton>
+                      <DeleteIcon />
+                    </IconButton>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <AddTaskDialog
+          onVisibilityChange={setVisibility}
+          visible={visible}
+          onSubmit={handleOnSubmit}
+        />
+      </>
+    );
+  }
+);
+
+const mapDispatchToProps = (dispatch: ThunkDispatch<{}, {}, any>) => ({
+  fetchAddTask: (title: string, description: string, userId: string) =>
+    dispatch(fetchAddTask(title, description, userId)),
+});
+
+export default connect(null, mapDispatchToProps)(TaskTable);
